4d77fc25200438852527fecb67770b892d930172
Maksym <50985690+MaksymPiavchyk@users.noreply.github.com>
2020-01-22 14:16:45 +0200

    In this Task I calculate perimeter of circle

---
+++ math fun/circle-P.py
@@ -0,0 +1,22 @@
+from flask import escape
+from math import pi
+def circle_P(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    request_json = request.get_json()
+    request_args = request.args
+    if request.args and 'r' in request.args:
+        r = request_args['r']
+        P = 2 * pi * r
+    elif request_json and 'r' in request_json:
+        r = request_json['r']
+        P = 2 * pi * r
+    else:
+        P = 'Error code'
+    return escape(P)
