b393e13030f31e40ae44723f43861d145d2f6a10
Maksym <50985690+MaksymPiavchyk@users.noreply.github.com>
2020-01-22 13:51:54 +0200

    Calculate perimeter of square

---
+++ square_P.py
@@ -0,0 +1,22 @@
+from flask import escape
+def square_P(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    request_json = request.get_json()
+    request_args = request.args
+    
+    if request.args and 'a' in request.args:
+        a = request_args['a']
+        P = 4 * a
+    elif request_json and 'a' in request_json:
+        a = request_json['a']
+        P = 4 * a
+    else:
+        P = 'Error code'
+    return escape(P)
